ff8ba73e7c43cde98b373eae4cc454179fdcc77c
agnarrarendelle <danny20001102@gmail.com>
2023-01-30 18:21:08 +0800

    moved footer to different file

---
+++ components/Footer.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { View, TextInput, TouchableOpacity, StyleSheet } from "react-native";
+import Icon from "react-native-vector-icons/MaterialIcons";
+
+type Prop = {
+  textInput: string;
+  setTextInput: (text: string) => void;
+  addTodo: () => void;
+};
+
+const Footer = (prop: Prop) => {
+  return (
+    <View style={styles.footer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          value={prop.textInput}
+          placeholder="Add Todo"
+          onChangeText={(text) => prop.setTextInput(text)}
+        />
+      </View>
+      <TouchableOpacity onPress={prop.addTodo}>
+        <View style={styles.iconContainer}>
+          <Icon name="add" color="white" size={30} />
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  footer: {
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 20,
+    backgroundColor: "#fff",
+  },
+  inputContainer: {
+    height: 50,
+    paddingHorizontal: 20,
+    elevation: 40,
+    backgroundColor: "#fff",
+    flex: 1,
+    marginVertical: 20,
+    marginRight: 20,
+    borderRadius: 30,
+  },
+  iconContainer: {
+    height: 50,
+    width: 50,
+    backgroundColor: "#1f145c",
+    elevation: 40,
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+export default Footer;
