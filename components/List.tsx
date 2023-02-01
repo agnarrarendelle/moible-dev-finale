b9fca5becd67824b7188712ca4e5de3dcc98690f
agnarrarendelle <danny20001102@gmail.com>
2023-02-01 18:45:47 +0800

    change prop

--- components/List.tsx
+++ components/List.tsx
@@ -1,25 +1,31 @@
-import { FlatList } from "react-native";
+import { FlatList, View } from "react-native";
+import Dropdown from "./Dropdown";
 import ListItem from "./ListItem";
 type Prop = {
   todos: Todo[];
   markTodoComplete: (id: string) => void;
   deleteTodo: (id: string) => void;
+  sortBy:(option:string)=>void
+
 };
 const List = (prop: Prop) => {
   return (
-    <FlatList
-      showsVerticalScrollIndicator={true}
-      contentContainerStyle={{ padding: 10 }}
-      data={prop.todos}
-      renderItem={({ item, index }) => (
-        <ListItem
-          todo={item}
-          markTodoComplete={prop.markTodoComplete}
-          deleteTodo={prop.deleteTodo}
-          index={index + 1}
-        />
-      )}
-    />
+    <View>
+      <Dropdown sortBy={prop.sortBy}></Dropdown>
+      <FlatList
+        showsVerticalScrollIndicator={true}
+        contentContainerStyle={{ padding: 10 }}
+        data={prop.todos}
+        renderItem={({ item, index }) => (
+          <ListItem
+            todo={item}
+            markTodoComplete={prop.markTodoComplete}
+            deleteTodo={prop.deleteTodo}
+            index={index + 1}
+          />
+        )}
+      />
+    </View>
   );
 };
 
