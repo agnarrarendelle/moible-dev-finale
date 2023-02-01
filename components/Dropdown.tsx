94a94c25148eeeab83f8a268851b40468b995d47
agnarrarendelle <danny20001102@gmail.com>
2023-02-01 18:45:35 +0800

    added a callback to sort list when pressed

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 import DropDownPicker from "react-native-dropdown-picker";
 
-const Dropdown = () => {
+type Prop={
+    sortBy:(option:string)=>void
+}
+
+const Dropdown = (prop:Prop) => {
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState(null);
   const [items, setItems] = useState([
     { label: "Sort by", value: "sort" },
     { label: "Name", value: "name", parent: "sort" },
-    { label: "Date", value: "Date", parent: "sort" },
+    { label: "Date", value: "date", parent: "sort" },
   ]);
   return (
     <DropDownPicker
@@ -17,6 +21,7 @@ const Dropdown = () => {
       setOpen={setOpen}
       setValue={setValue}
       setItems={setItems}
+      onChangeValue={(v)=>prop.sortBy(v!)}
     />
   );
 };
