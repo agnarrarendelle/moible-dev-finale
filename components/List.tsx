77ad1f760d5803c92a8711e28274fdd115d4fe84
agnarrarendelle <danny20001102@gmail.com>
2023-02-01 20:00:05 +0800

    changed string to enum values

--- components/List.tsx
+++ components/List.tsx
@@ -1,21 +1,34 @@
 import { FlatList, View } from "react-native";
 import Dropdown from "./Dropdown";
 import ListItem from "./ListItem";
+import { FilterOptions } from "../constant";
 type Prop = {
   todos: Todo[];
+  filterOption:string,
   markTodoComplete: (id: string) => void;
   deleteTodo: (id: string) => void;
   sortBy:(option:string)=>void
 
 };
 const List = (prop: Prop) => {
+  const getFilteredList=()=>{
+    switch(prop.filterOption){
+      case FilterOptions.All:
+        return prop.todos
+      case FilterOptions.Completed:
+        return prop.todos.filter(todo=>todo.isCompleted === true)
+      case FilterOptions.InCompleted:
+        return prop.todos.filter(todo=>todo.isCompleted === false)
+    }
+  }
+
   return (
     <View>
       <Dropdown sortBy={prop.sortBy}></Dropdown>
       <FlatList
         showsVerticalScrollIndicator={true}
         contentContainerStyle={{ padding: 10 }}
-        data={prop.todos}
+        data={getFilteredList()}
         renderItem={({ item, index }) => (
           <ListItem
             todo={item}
