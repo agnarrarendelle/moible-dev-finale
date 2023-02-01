b5a4665aaa84ef3efd3915789185c4ff9040d4ff
agnarrarendelle <danny20001102@gmail.com>
2023-02-01 19:59:31 +0800

    changed string to enum values

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import DropDownPicker from "react-native-dropdown-picker";
-
+import { FilterOptions } from "../constant";
 type Prop={
     sortBy:(option:string)=>void
 }
@@ -12,6 +12,10 @@ const Dropdown = (prop:Prop) => {
     { label: "Sort by", value: "sort" },
     { label: "Name", value: "name", parent: "sort" },
     { label: "Date", value: "date", parent: "sort" },
+    { label: "Filter tasks", value: "filter task" },
+    { label: "Completed", value: FilterOptions.Completed, parent: "filter task" },
+    { label: "Incompleted", value: FilterOptions.InCompleted, parent: "filter task" },
+    { label: "All", value: FilterOptions.All, parent: "filter task" },
   ]);
   return (
     <DropDownPicker
