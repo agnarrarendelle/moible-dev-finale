450a4599c3e6e2389273a5bcdf22b44ed8006588
agnarrarendelle <danny20001102@gmail.com>
2023-02-02 17:11:55 +0800

    Make list detail take an optional detail arg

--- global.d.ts
+++ global.d.ts
@@ -14,6 +14,7 @@ declare global {
     ListDetail: {
       id: string;
       task: string;
+      detail?: string;
       setTaskDetail: (id: string,newTask:string, newDetail: string) => void;
     };
   };
