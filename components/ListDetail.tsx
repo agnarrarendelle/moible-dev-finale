b59a76ae37bb4567ea7408fa4cd9d0a8ccd9aaf9
agnarrarendelle <danny20001102@gmail.com>
2023-02-02 13:55:40 +0800

    added a component to show list detail

---
+++ components/ListDetail.tsx
@@ -0,0 +1,17 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import React from "react";
+import { View, Text, Button } from "react-native";
+type ListDetailScreenProp = NativeStackScreenProps<
+  StackParamList,
+  "ListDetail"
+>;
+const ListDetail: React.FC<ListDetailScreenProp> = (prop) => {
+  return (
+    <View>
+      <Text>{prop.route.params.id}</Text>
+      <Button title="test" onPress={()=>prop.navigation.navigate("Home")}></Button>
+    </View>
+  );
+};
+
+export default ListDetail;
