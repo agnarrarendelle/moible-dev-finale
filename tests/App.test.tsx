d7d2d8be32d00a02feaeddf9e4b89fbc9d5bf8d3
agnarrarendelle <danny20001102@gmail.com>
2023-02-05 14:04:08 +0800

    added first test file

---
+++ tests/App.test.tsx
@@ -0,0 +1,9 @@
+import { describe, expect, test } from "@jest/globals";
+import renderer from 'react-test-renderer';
+import App from "../App";
+describe('<App />', () => {
+  it('has 1 child', () => {
+    const tree = renderer.create(<App/>).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
