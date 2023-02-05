f845527d5b64d8c1a4f265a9f87be909a9dd7680
agnarrarendelle <danny20001102@gmail.com>
2023-02-05 14:04:49 +0800

    .

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -2,7 +2,7 @@ import { describe, expect, test } from "@jest/globals";
 import renderer from 'react-test-renderer';
 import App from "../App";
 describe('<App />', () => {
-  it('has 1 child', () => {
+  it('test App snapshot', () => {
     const tree = renderer.create(<App/>).toJSON();
     expect(tree).toMatchSnapshot();
   });
